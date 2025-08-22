65c7e67a0dbe446344f0f250d4cce5af4bb0e0d0
Franco Sappa <sappa57@gmail.com>
2025-08-22 19:51:25 -0300

    Ciclo WHILE
    Misma practica usando ciclo WHILE

---
+++ Semana_1/ciclo_WHILE.py
@@ -0,0 +1,48 @@
+print("Bienvenido al Parque de Diversiones PythoLand!!")
+nombre = input("Ingresa tu nombre: ")
+edad = int(input("Ingresa tu edad: "))
+
+print("\n-Atracciones-\n"+"Montaña Rusa $1500\n"+"Casa del Terror $1200\n"+"Carrusel $800")
+
+pago_total = 0
+lista_atracciones = []
+atracciones_permitidas = []
+
+while True:
+    atraccion = input("Ingrese la atraccion: ").capitalize()
+    lista_atracciones.append(atraccion)
+    match atraccion:
+        case "Montaña rusa":
+                if edad >= 12:
+                    print("Puede subir")
+                    pago_total += 1500
+                    atracciones_permitidas.append("Montaña rusa")
+                else:
+                    print("No se admiten menores de 12 años")
+        case "Casa del terror":
+                if edad > 6:
+                    print("Puede subir")
+                    pago_total += 1200
+                    atracciones_permitidas.append("Casa del terror")
+                else:
+                    print("No se admiten menores de 6 años")
+        case "Carrusel":
+                print("Puede subir")
+                pago_total += 800
+                atracciones_permitidas.append("Carrusel")
+        case _:
+                print("Atraccion inexistente")
+                lista_atracciones.remove(atraccion)
+    
+    pregunta = input("Queres subirte a otra atraccion? ").lower()
+    if pregunta == "si":
+          continue
+    else:
+          break
+
+print(f"\n-Resumen-\n"
+      f"Nombre: {nombre}\n"
+      f"Atracciones Elegidas: {lista_atracciones}\n"
+      f"Atracciones Permitidas: {atracciones_permitidas}\n"
+      f"Total a pagar: {pago_total}"
+)

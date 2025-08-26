129db5fee7ea58dbe4d8f30ae3023605ee75cc7c
Franco Sappa <sappa57@gmail.com>
2025-08-26 17:00:04 -0300

    Update ejercicio ciclo for

--- Ejercicios_Extras/Ciclos/FOR/ciclo_for_9.py
+++ Ejercicios_Extras/Ciclos/FOR/ciclo_for_9.py
@@ -3,7 +3,7 @@ cantidad_divisores = 0
 
 for x in range(1, numero + 1):
     if numero %x == 0:
-        print(f"Divisores: {x}")
+        print(x)
         cantidad_divisores += 1
 
 print(f"Cantidad Divisores: {cantidad_divisores}")

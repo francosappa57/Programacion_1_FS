da794341660fd8ba143129edcc8b81d7d947deae
Franco Sappa <sappa57@gmail.com>
2025-08-27 08:43:38 -0300

    Update ejercicios funciones

--- Semana_1/Funciones/funcion_6.py
+++ Semana_1/Funciones/funcion_6.py
@@ -2,13 +2,10 @@ def convertir_minutos(minutos):
     hora = 0
     minuto = 0
     for x in range(1, minutos + 1):
+        minuto += 1
         if x %60 == 0:
             hora += 1
-        else:
-            minuto += 1
-            if minuto == 59:
-                minuto -= 59
-    
+            minuto -= 60
     return hora, minuto
         
 tiempo = int(input("Ingresa la hora en minutos: "))

67e34d6412bc81163c334408edf756343c3b4c47
Franco Sappa <sappa57@gmail.com>
2025-09-15 13:04:00 -0300

    Practica array semana 5

--- Semana_5/funciones.py
+++ Semana_5/funciones.py
@@ -1,36 +1,56 @@
-CANT_LIBROS = 2
+CANT_LIBROS = 4
+libros = [""] * CANT_LIBROS
+copias = [0] * CANT_LIBROS
 
 def cargar_libro():
-    libros = [""] * CANT_LIBROS
-    copias = [0] * CANT_LIBROS
-    
     for i in range(CANT_LIBROS):
         ejem = input("Ingresa un libro: ")
         cant = int(input("Ingresa cantidad de ejemplares: "))
         libros[i] = ejem
         copias[i] = cant
+        pregunta = input("Queres agregar otro libro? (s/n) ").lower()
+        if pregunta == "n":
+            break
     return libros, copias
 
 
 def mostrar_libros(libro, copia):
     for i in range(len(libro)):
-        print(f"{libro[i]} -> {copia[i]} copias", end="\n")
+        if libro[i] == "":
+            pass
+        else:
+            print(f"{libro[i]} -> {copia[i]} copias", end="\n")
 
 
 def disponible(consulta, libro, copia):
     for i in range(len(libro)):
         if libro[i] == consulta:
-            return copia[i]
+            print(f"Quedan {copia[i]} copias")
 
 
 def sin_copias(libro, copia):
-    datos = ""
-    for i in range(len(libro)):
-        if copia[i] == 0:
-            datos += libro[i]
-    return datos
+    print("Libros sin copias disponibles:")
+    for i in range(CANT_LIBROS):
+        if libro[i] != "":
+            if copia[i] == 0:
+                print(libro[i])
 
 
 def agregar_titulo(libro, copia):
-    if len(libro) > CANT_LIBROS:
-        cargar_libro
+    for i in range(CANT_LIBROS):
+        if libro[i] == "":
+            nuevo_libro = input("Ingresa nuevo libro: ")
+            nueva_copia = int(input("Ingresa cantidad de copias: "))
+            libro[i] = nuevo_libro
+            copia[i] = nueva_copia
+            print("Libro agregado")
+        else:
+            print("No se pueden agregar nuevos libros")
+
+
+def actualizar(cambio, libro, copia):
+    for i in range(CANT_LIBROS):
+        if libro[i] == cambio:
+            cambiar_cant = int(input("Ingresa nueva cantidad: "))
+            copia[i] = cambiar_cant
+            print("Copias modificadas")
--- Semana_5/main.py
+++ Semana_5/main.py
@@ -1,33 +1,34 @@
 import funciones as f
-CANT_LIBROS = 2
-libros = [""] * CANT_LIBROS
-copias = [0] * CANT_LIBROS
 
 while True:
-    print("---Biblioteca---\n")
-    print("1. Cargar libro y cantidad\n",
-          "2. Mostrar Catalogo\n",
-          "3. Ver libros disponibles\n",
-          "4. Ver libros agotados\n",
-          "5. Agregar nuevo libro\n",
-          "6. Actualizar cantidad libros\n",
+    print("\t---Biblioteca---")
+    print("1. Cargar libro y cantidad\n"
+          "2. Mostrar Catalogo\n"
+          "3. Mostrar cantidad de copias\n"
+          "4. Ver libros agotados\n"
+          "5. Agregar nuevo libro\n"
+          "6. Actualizar cantidad copias libros\n"
           "7. Salir")
     datos = int(input("Elige una opcion: "))
 
     if datos == 1:
-        ejem, cant = f.cargar_libro()
-         
+        ejem, cant = f.cargar_libro() 
     elif datos == 2:
         f.mostrar_libros(ejem, cant)
     elif datos == 3:
         pedir_titulo = input("Ingresa titulo a consultar: ")
-        disp = f.disponible(pedir_titulo, ejem, cant)
-        print(f"Quedan {disp} copias")
+        f.disponible(pedir_titulo, ejem, cant)
     elif datos == 4:
-        sin_copias = f.sin_copias(ejem, cant)
-        print("---Libro/s sin copia/s---")
-        print(f"{sin_copias}")
+        f.sin_copias(ejem, cant)
     elif datos == 5:
+        f.agregar_titulo(ejem, cant)
+    elif datos == 6:
+        print(ejem)
+        pedir_libro = input("Que libro queres modificar? ")
+        f.actualizar(pedir_libro, ejem, cant)
+    elif datos == 7:
+        break
+
 
 
 

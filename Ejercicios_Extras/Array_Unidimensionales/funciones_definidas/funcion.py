b3efbd3370ace50162c05db95d1684cf9a728f33
Franco Sappa <sappa57@gmail.com>
2025-09-19 08:39:57 -0300

    Update ejercicio array unidimensional

--- Ejercicios_Extras/Array_Unidimensionales/funciones_definidas/funcion.py
+++ Ejercicios_Extras/Array_Unidimensionales/funciones_definidas/funcion.py
@@ -55,14 +55,23 @@ def posicion_valor_max(vec):
     return posicion
 
 
-def posiciones(vec):
-    maximo = 0
-    indice = ""
-    for x in range(len(vec)):
-        if vec[x] >= maximo:
-            maximo = vec[x]
-            indice += str(x)
-    return indice
+# def posiciones(vec):
+#     lista = [0] * len(vec)
+#     maximo = None
+#     indice = None
+#     for x in range(len(vec)):
+#         if x == 0:
+#             maximo = vec[x]
+#             indice = x
+#             lista[indice] = x
+#         elif vec[x] > maximo:
+#             maximo = vec[x]
+#             indice = 0
+#             lista[indice] = x
+#         elif vec[x] == maximo:
+#             lista[indice] = x
+#         indice += 1
+#     return lista
  
 
 def reemplazar_nombre(lista_nombres, nombre_antiguo, nombre_nuevo):
